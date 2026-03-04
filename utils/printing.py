7fcefab7acf41869c21e62b8f90c8df9223782a2
UriKH <urikh@icloud.com>
2026-03-04 13:33:52 +0200

    update printing

--- utils/printing.py
+++ utils/printing.py
@@ -1,7 +1,8 @@
 def print_title(msg, n=100):
     print()
-    print(n * "=")
+    n = (n // 2) * 2
+    print((n // 2) * "===")
     t = (n - len(msg)) // 2
     delta = n - 2 * t - len(msg)
-    print(((t-1) * "=") + f' {msg} ' + ((t-1 + delta) * "="))
-    print(n * "=")
+    print(("#" + (t-1) * " ") + f'{msg}' + ((t-1 + delta) * " " + "#"))
+    print((n // 2) * "===")
