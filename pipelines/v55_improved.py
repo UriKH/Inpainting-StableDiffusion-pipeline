730e14d3142ce63980ac6b1ca7034d4537b1029d
shaked18 <shaked-adi@cs.technion.ac.il>
2026-03-11 12:51:24 +0200

    fix

--- pipelines/v55_improved.py
+++ pipelines/v55_improved.py
@@ -7,10 +7,11 @@ import numpy as np
 
 
 class ImprovedInpaintPipelineV55(ImprovedInpaintPipelineV3):
-    def __init__(self, dilate_kernel_size=3, feather_radius=5, *args, **kwargs):
+    def __init__(self, pp_dilate_kernel_size=3, pp_feather_kernel_size=5, use_negative_prompt=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.dilate_kernel_size = dilate_kernel_size
-        self.feather_radius = feather_radius
+        self.pp_dilate_kernel_size = pp_dilate_kernel_size
+        self.pp_feather_kernel_size = pp_feather_kernel_size
+        self.use_negative_prompt = use_negative_prompt
 
     def encode_prompt(self, prompt, text_encoder, tokenizer):
         text_input = tokenizer(
@@ -19,7 +20,7 @@ class ImprovedInpaintPipelineV55(ImprovedInpaintPipelineV3):
         text_embeddings = text_encoder(text_input.input_ids.to(self.device))[0]
         uncond_input = tokenizer(
             [
-                "ugly, tiling, poorly drawn, out of frame, deformed, blurry, bad anatomy, bad proportions, extra limbs, artifacts, miniature scene, entire picture, out of context, mismatched lighting"
+                "ugly, tiling, poorly drawn, out of frame, deformed, blurry, bad anatomy, bad proportions, extra limbs, artifacts, miniature scene, entire picture, out of context, mismatched lighting" if self.use_negative_prompt else ""
             ],
             padding="max_length", max_length=tokenizer.model_max_length, return_tensors="pt"
         )
@@ -32,10 +33,12 @@ class ImprovedInpaintPipelineV55(ImprovedInpaintPipelineV3):
         Enhances the mask using Dilation and Feathering to prevent 'cut' edges.
         """
         mask_np = np.array(mask_image.convert("L"))
-        kernel = np.ones((self.dilate_kernel_size, self.dilate_kernel_size), np.uint8)
+        kernel = np.ones((self.pp_dilate_kernel_size, self.pp_dilate_kernel_size), np.uint8)
         mask_dilated = cv.dilate(mask_np, kernel, iterations=1)
-        
-        mask_pil = Image.fromarray(mask_dilated).filter(ImageFilter.GaussianBlur(radius=self.feather_radius))
+        if self.pp_feather_kernel_size != 1:
+            mask_pil = Image.fromarray(mask_dilated).filter(ImageFilter.GaussianBlur(radius=self.pp_feather_kernel_size))
+        else:
+            mask_pil = Image.fromarray(mask_dilated)
         return mask_pil
 
     def preprocess(self, pipe_in: InpaintPipelineInput):
