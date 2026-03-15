5a289251b36e7d17953cd6808be5c772b6957efe
UriKH <urikh@icloud.com>
2026-03-15 22:10:50 +0200

    remove mask save

--- code/our_runner.py
+++ code/our_runner.py
@@ -69,8 +69,6 @@ class OurDatasetGenerator:
             mask_image = Image.fromarray(mask_image).convert("L")
             pipe_in = InpaintPipelineInput(prompt, init_image, mask_image)
 
-            # remove this later
-            pipe_in.init_image.save(os.path.join(output_dir, f'{img_id}_init.png'))
             result_img = pipeline.pipe(pipe_in)
             out_path = os.path.join(output_dir, filename)
             result_img.save(out_path)
